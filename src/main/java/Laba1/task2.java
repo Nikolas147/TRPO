1c29f9d0263237e376dcc269338d8973145c4f96
Алексей <mister.pim97@yandex.ru>
2018-03-06 11:28:38 +0300

    new regex

--- src/main/java/Laba1/task2.java
+++ src/main/java/Laba1/task2.java
@@ -19,11 +19,11 @@ public class task2 {
     public static void main(String args[]) throws IOException {
         String regexData     = "(0[1-9]|1[0-9]|2[0-9]|3[01])-(0[1-9]|1[0-2])-(19|20|21)[0-9]{2}";
         String regexDataTime = "(0[1-9]|1[0-9]|2[0-9]|3[01])-(0[1-9]|1[0-2])-(19|20|21)[0-9]{2}T(0[0-9]|1[0-9]|2[0-4])(:([0-5][0-9])){2}";
-        String regexMail     = "([\\S]{1,20})@([a-zA-Z]{1,20}).(com|ru|org)";
+        String regexMail     = "([a-zA-Z]{1})+([-\\w.]{0,62})@([A-z0-9][-A-z0-9]+\\.)+[A-z]{2,4}";
 
         String string1       = "20-12-2133";
         String string2       = "dsf 01-12-2012 sdfpfdzsxgvjlgdfzftreweq2356786redxcvb 20-12-2133T18:50:50";
-        String string3       = "ddcdfkjg___fdf.hjklj@drom.com";
+        String string3       = "ddcd_fkjgfdf.hjklj@drom.com";
 
         isRegex(regexData, string1);
         isRegex(regexDataTime, string2);
